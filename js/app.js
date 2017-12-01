6f07c98a729005d29aef3ea267304ae55f5f3f80
Jarvis Smith <jarvis_smith@charter.net>
2017-12-01 15:59:40 -0800

    Much better

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
-/*
- * Create a list that holds all of your cards
- */
-
-let lastCard, lastIndex, cardStatus, matches, cardPick, moveCount;
+let cardIndex, lastIndex, matches, moveCount;
 let noclick = false;
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the timer
+var timer = document.getElementById('timer');
+var timerVar = setInterval(gameTimer, 1000);
+let seconds, minutes;
+
+let openCards = [];
+
 let cards = [
     "fa-diamond",
     "fa-paper-plane-o",
@@ -17,15 +24,20 @@ let cards = [
 cards = cards.concat(cards);
 
 function initGame() {
+    clearInterval(timerVar)
+    minutes = 0;
+    seconds = 0;
+    startTimer = false;
     matches = 0;
     cardPick = 0;
     moveCount = 0;
     shuffle(cards);
     $('.deck').empty();
     $('.stars').empty();
+    $('#timer').html('0:00');
     $('body > div > section > span').text('0');
 
-    for (var index = 0; index < cards.length; index++){
+    for (let index = 0; index < cards.length; index++){
         $('.deck').append('<li class="card"><i class="fa ' + cards[index] + '"></i></li>');
     }
 }
@@ -52,69 +64,116 @@ function shuffle(array) {
     return array;
 }
 
-initGame();
-mainLoop();
-
-function clearLastCards(last,curcard) {
-    noclick = true;
-    setTimeout('$(\"li:nth-child(' + last + ') > i\").parent().toggleClass(\"open show\");',2000);
-    setTimeout('$(\"li:nth-child(' + curcard + ') > i\").parent().toggleClass(\"open show\");',2000);
-    setTimeout("noclick = false;",2000);
+function gameTimer() {
+        var d = new Date();
+        seconds++;
+        if (seconds===60) {
+            seconds=0;
+            minutes++;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        document.getElementById("timer").innerHTML = minutes + ":" + seconds;
 }
 
+function clearLastCards(lastcard,curcard) {
+    noclick = true;
+    openCards = [];
+    $('li:nth-child(' + lastcard + ') > i').parent().toggleClass("open wrong");
+    $('li:nth-child(' + curcard + ') > i').parent().toggleClass("open wrong");
+    setTimeout('$(\"li:nth-child(' + lastcard + ') > i\").parent().toggleClass(\"open wrong\");',1000);
+    setTimeout('$(\"li:nth-child(' + curcard + ') > i\").parent().toggleClass(\"open wrong\");',1000);
+    setTimeout('$(\"li:nth-child(' + lastcard + ') > i\").parent().toggleClass(\"open show\");',1000);
+    setTimeout('$(\"li:nth-child(' + curcard + ') > i\").parent().toggleClass(\"open show\");',1000);
+    setTimeout("noclick = false;",1000);
+}
+
+function displayCard(cardIndex) {
+    $('li:nth-child(' + cardIndex + ') > i').parent().toggleClass('open show');
+}
+
+function addOpenCardList(cardIndex) {
+    openCards.push(cards[cardIndex - 1]);
+}
+
+function lockMatch(lastcard,curcard) {
+    $('.stars').append('<li><i class="fa fa-star"></li>');
+    $('li:nth-child(' + curcard + ') > i').parent().toggleClass('match');
+    $('li:nth-child(' + lastcard + ') > i').parent().toggleClass('match');
+    lastIndex = -1;
+    matches++;
+    openCards = [];
+}
+
+function countMove() {
+    moveCount++;
+    $('body > div > section > span').text(moveCount);
+    if (moveCount === 1) {timerVar = setInterval(gameTimer, 1000);}
+}
+
+function youWon() {
+    clearInterval(timerVar);
+    $('#myModal > div > div.modal-body').empty();
+    $('#myModal > div > div.modal-body').html('<p>Your time was ' + minutes + ':' + seconds + '</p><p>It took you ' + moveCount + ' moves to complete the game</p>');
+    modal.style.display = "block";
+    $('#myModal > div > div.modal-header > span').click(function(){
+        modal.style.display = "none";
+        location.reload();
+    });
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            location.reload();
+        }
+    }
+}
 
 function mainLoop() {
 
     $('.restart').click(function(){
-        initGame();
-        mainLoop();
+        location.reload();
     });
 
+//* set up the event listener for a card. If a card is clicked:
     $('body > div > ul > li').click(function(){
-        console.log(cardPick);
         if (noclick) {return};
         cardIndex = parseInt( $(this).index() ) + 1;
-        cardPick++;
-        moveCount++;
+
+//*  - display the card's symbol (put this functionality in another function that you call from this one)
         if ($('li:nth-child(' + cardIndex + ') > i').parent().hasClass('open')) {
             return;
-        } else $('li:nth-child(' + cardIndex + ') > i').parent().toggleClass('open show');
+        } else displayCard(cardIndex);
 
-        $('body > div > section > span').text(moveCount);
-        //console.log(cardIndex + ' ' + cards[cardIndex - 1]);
-        //console.log('Picked card ' + cardPick + ' of 2');
+//*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+        addOpenCardList(cardIndex);
 
-        if ((cardPick === 2) && (cards[cardIndex - 1] === lastCard) && (cardIndex != lastIndex)) {console.log('Match!');
-            $('.stars').append('<li><i class="fa fa-star"></li>');
-            $('li:nth-child(' + cardIndex + ') > i').parent().toggleClass('match');
-            $('li:nth-child(' + lastIndex + ') > i').parent().toggleClass('match');
-            lastCard = '';
-            lastIndex = -1;
-            cardPick = 0;
-            matches++;
+//*  - if the list already has another card, check to see if the two cards match
+//*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+
+        if ((openCards.length === 2) && (openCards[1] === openCards[0])) {
+            lockMatch(lastIndex,cardIndex);
         }
 
-        lastCard = cards[cardIndex - 1];
-        //lastIndex = cardIndex;
-
-        if ((cardPick === 2) && (matches != 8)) {
-            cardPick = 0;
+//*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+        if ((openCards.length === 2) && (matches != 8)) {
             clearLastCards(lastIndex,cardIndex);
         }
 
+//*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+        countMove();
+
+//*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
         if (matches === 8) {
-            cardPick = 0;
-            alert('You Won');
-            initGame();
-            mainLoop();
+            youWon();
         }
-
         lastIndex = cardIndex;
-
     });
 }
 
-
+initGame();
+mainLoop();
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
